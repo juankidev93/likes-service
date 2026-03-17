fbe4ed8e5a1c5f0b20fba657df58f4367b1d1f95
juankidev93 <juancarlosgq93@gmail.com>
2026-03-17 10:40:33 +0000

    feat: add graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -40,10 +40,43 @@ async fn main() {
         .await
         .expect("failed to bind TCP listener");
 
+    tracing::info!(address = %config.bind_address(), "starting HTTP server");
+
     axum::serve(
         listener,
         app.into_make_service_with_connect_info::<SocketAddr>(),
     )
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .expect("HTTP server failed");
+
+    tracing::info!("HTTP server stopped");
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C signal handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("failed to install SIGTERM signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {
+            tracing::info!("received Ctrl+C, starting graceful shutdown");
+        }
+        _ = terminate => {
+            tracing::info!("received SIGTERM, starting graceful shutdown");
+        }
+    }
 }
